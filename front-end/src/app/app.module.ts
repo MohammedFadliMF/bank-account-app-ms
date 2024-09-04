d0dc88e8568a3e9b3e52d510302069041f54f3fb
FADLIMohammed <120965149+FADLIMohammed@users.noreply.github.com>
2024-09-04 11:31:02 +0000

    Add front-end

---
+++ front-end/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+import { AccountsComponent } from './accounts/accounts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomersComponent,
+    AccountsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
